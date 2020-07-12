12847a0b93284568931e19a117605187229618ca
tubone24 <tubo.yyyuuu@gmail.com>
2020-07-12 19:37:08 +0900

    background

--- src/main.py
+++ src/main.py
@@ -103,9 +103,9 @@ def remove_url(text):
 
 
 def overdraw_image():
-    im1 = Image.open("mask_photos/head-profile-of-young-male.png")
+    im1 = Image.open("background/3504359.jpg")
     im2 = Image.open(os.path.join(os.path.dirname(__file__), "word_cloud_tweet_face_profile_alpha.png"))
-    im1.paste(im2)
+    im1.paste(im2, (1000, 500))
     im1.save(os.path.join(os.path.dirname(__file__), "word_cloud_tweet_face_profile_overlay.png"), quality=95)
 
 

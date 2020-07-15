63845fa8b767d39c4d81cb4c4bc13fa9460d9c6d
tubone24 <tubo.yyyuuu@gmail.com>
2020-07-16 00:46:27 +0900

    get all tweet

--- src/main.py
+++ src/main.py
@@ -69,10 +69,7 @@ def get_tweets():
         new_tweets = api.user_timeline(screen_name=SCREEN_NAME, count=200, max_id=oldest, include_rts=False, exclude_replies=True)
         all_tweets.extend(new_tweets)
         oldest = all_tweets[-1].id - 1
-        print(f"...{len(all_tweets)} tweets downloaded so far")
     return [remove_emoji(x['text']) for x in all_tweets]
-
-
 
 
 def remove_emoji(src_str):

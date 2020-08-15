1aae21fe8bd88610095213f9c5211a1716dc11bb
tubone24 <tubo.yyyuuu@gmail.com>
2020-08-16 00:10:37 +0900

    :recycle: Retries with Exponential Backoff

--- src/tweet.py
+++ src/tweet.py
@@ -41,10 +41,10 @@ class Tweet:
     def get_trends_tokyo(self):
         return [remove_emoji(x["name"]) for x in self.api.trends_place(1118285)[0]["trends"]]
 
-    def get_friends(self, user):
+    def get_followers(self, user):
         followers = []
         try:
-            for follower in self.__limit_handled(tweepy.Cursor(self.api.friends, user).items()):
+            for follower in self.__limit_handled(tweepy.Cursor(self.api.followers, user).items()):
                 followers.append(follower)
         except RuntimeError as e:  # RuntimeError: generator raised StopIteration
             print(e)
@@ -52,13 +52,13 @@ class Tweet:
 
     def get_followers_followers_list(self):
         followers_followers_list = []
-        my_followers = self.get_friends(SCREEN_NAME)
+        my_followers = self.get_followers(SCREEN_NAME)
         followers_followers_list.append([follower.screen_name for follower in my_followers])
         for follower in my_followers:
             print(follower.screen_name)
             if follower.friends_count > 300:
                 print("many friends may be a spam: {}".format(follower.friends_count))
                 continue
-            others_followers = self.get_friends(follower.screen_name)
+            others_followers = self.get_followers(follower.screen_name)
             followers_followers_list.append([follower.screen_name for follower in others_followers])
         return followers_followers_list

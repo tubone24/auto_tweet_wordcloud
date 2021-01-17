12242677b10b57bde02c710917bf27cc754a4dcf
tubone <meitantei-conan@live.jp>
2021-01-17 06:49:37 +0000

    compress

--- src/convert_webp.py
+++ src/convert_webp.py
@@ -12,4 +12,4 @@ def conv_webp(file_path):
     if file_type.lower() in img_formats:
         img = Image.open(file_path)
         webp = file_path.replace(file_type, ".webp")
-        img.save(webp, "WEBP")
+        img.save(webp, "WEBP", quality=50, method=6)
